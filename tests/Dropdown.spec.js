025841b4ed9c97c7b0344f9033b4506feb63e288
sazidahmednassir <sazidahmednassir@gmail.com>
2024-12-04 02:00:34 +0600

    Updated the dropdown code

--- tests/Dropdown.spec.js
+++ tests/Dropdown.spec.js
@@ -2,12 +2,17 @@ import { test, expect } from '@playwright/test';
 
 test("Another Login test", async({page})=>{
 
+    
+
     await page.goto("https://rahulshettyacademy.com/angularpractice/")
-    await page.fill(".form-group>>[name='name']", "salek")
-    await page.fill(".form-group>>[name='email']", "salek23@yopmail.com")
+    await page.fill(".form-group>>[name='name']", "salekin")
+    await page.fill(".form-group>>[name='email']", "salekin23@yopmail.com")
     await page.fill("#exampleInputPassword1", "test")
     await page.check("#exampleCheck1")
     await page.check("//label[@for='inlineRadio1']")
     await page.selectOption("#exampleFormControlSelect1", "Male")
-    await page.waitForTimeout(5000)
+    await page.fill("//input[@name='bday']", '2024-11-24');
+    await page.click("[type='submit']")
+    // await page.waitForTimeout(5000)
+    await expect(page.locator('text=Success! The Form has been submitted successfully!.')).toBeVisible();
 })

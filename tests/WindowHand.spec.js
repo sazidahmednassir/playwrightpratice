41a552c3e6f90e12ad4b14804dad286557349c4f
sazidahmednassir <sazidahmednassir@gmail.com>
2024-12-12 17:47:24 +0600

    frame half code

--- tests/WindowHand.spec.js
+++ tests/WindowHand.spec.js
@@ -18,6 +18,34 @@ test("Child window", async({browser})=>{
     const result=await newpage.getByRole('heading', {name:'Documents request'}).isVisible()
     expect(result).toBeTruthy()
 
-    await newpage.waitForTimeout(3000)
+    await page.locator("[name='username']").fill("rahulshettyacademy")
+    await page.locator("[name='password']").fill("learning")
+    await page.locator("[type='submit']").click()
+
+    await newpage.waitForTimeout(2000)
 
 })   
+
+test("Child window handling another way", async({browser})=>{
+
+    const context= await browser.newContext();
+    const page2= await context.newPage();
+
+    await page2.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const [newpage2]= await Promise.all([
+         context.waitForEvent('page'),
+         page2.locator("[href*='documents-request']").click()
+    ])
+
+    await newpage2.getByRole('heading', {name:'Documents request'}).waitFor()
+    const result=await newpage2.getByRole('heading', {name:'Documents request'}).isVisible()
+    expect(result).toBeTruthy()
+
+    await page2.locator("[name='username']").fill("rahulshettyacademy")
+    await page2.locator("[name='password']").fill("learning")
+    await page2.locator("[type='submit']").click()
+
+    await newpage2.waitForTimeout(3000)
+
+})

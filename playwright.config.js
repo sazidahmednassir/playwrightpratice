d4a9ed601a12ab8ea8d708ef0c42106543199077
sazidahmednassir <sazidahmednassir@gmail.com>
2025-01-02 17:07:17 +0600

    Updated the platwright config file

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 10 : 20,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["line"], ["allure-playwright"]],
   // timeout: 240000, // Test timeout
